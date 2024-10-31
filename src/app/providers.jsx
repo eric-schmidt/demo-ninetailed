2fca2d778be1792faf3afbf2bd1db30bd2b56f71
Eric Schmidt <e.ryan.schmidt@gmail.com>
2024-10-31 11:54:34 -0600

    Conditionally add Ninetailed Preview Plugin only when Draft Mode is enabled.

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -12,31 +12,48 @@ export const Providers = ({
   draftModeEnabled,
   audiences,
   experiences,
-}) => (
-  <NinetailedProvider
-    clientId={process.env.NEXT_PUBLIC_NINETAILED_API_KEY}
-    environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT_ID}
-    plugins={[
-      new NinetailedInsightsPlugin(),
-      new NinetailedPreviewPlugin({
-        audiences: audiences,
-        experiences: experiences,
-        ui: { opener: { hide: false } },
-        // TODO: Enable these handlers.
-        // Optional: Callback to handle user forwarding to the experience entry in your CMS
-        // onOpenExperienceEditor: (experience) => {},
-        // Optional: Callback to handle user forwarding to the audience entry in your CMS
-        // onOpenAudienceEditor: (experience) => {},
-        // Optional: Determine the visibility of the Preview Plugin
-      }),
-    ]}
-  >
-    <ContentfulLivePreviewProvider
-      locale="en-US"
-      enableInspectorMode={true || draftModeEnabled}
-      enableLiveUpdates={true || draftModeEnabled}
+}) => {
+  console.log("DRAFT ENABLED?", draftModeEnabled);
+
+  return (
+    <NinetailedProvider
+      clientId={process.env.NEXT_PUBLIC_NINETAILED_API_KEY}
+      environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT_ID}
+      plugins={[
+        new NinetailedInsightsPlugin(),
+        ...(draftModeEnabled
+          ? [
+              new NinetailedPreviewPlugin({
+                audiences: audiences,
+                experiences: experiences,
+                ui: { opener: { hide: false } },
+                // Optional: Callback to handle user forwarding to the experience entry in your CMS
+                onOpenExperienceEditor: (experience) => {
+                  window.open(
+                    `https://app.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}/entries/${experience.id}`,
+                    "_blank"
+                  );
+                },
+                // Optional: Callback to handle user forwarding to the audience entry in your CMS
+                onOpenAudienceEditor: (audience) => {
+                  window.open(
+                    `https://app.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}/entries/${audience.id}`,
+                    "_blank"
+                  );
+                },
+                // Optional: Determine the visibility of the Preview Plugin
+              }),
+            ]
+          : []),
+      ]}
     >
-      {children}
-    </ContentfulLivePreviewProvider>
-  </NinetailedProvider>
-);
+      <ContentfulLivePreviewProvider
+        locale="en-US"
+        enableInspectorMode={true || draftModeEnabled}
+        enableLiveUpdates={true || draftModeEnabled}
+      >
+        {children}
+      </ContentfulLivePreviewProvider>
+    </NinetailedProvider>
+  );
+};
