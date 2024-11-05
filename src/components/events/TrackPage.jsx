92b96ab998d874217da0e6fd51983add101e639c
Eric Schmidt <e.ryan.schmidt@gmail.com>
2024-11-05 12:04:50 -0700

    Add query parameter tracking to TrackPage component.

--- src/components/events/TrackPage.jsx
+++ src/components/events/TrackPage.jsx
@@ -1,14 +1,28 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useNinetailed } from "@ninetailed/experience.js-react";
 
 export const TrackPage = () => {
   const pathname = usePathname();
-  const { page } = useNinetailed();
+  const { identify, page } = useNinetailed();
+  const personalizationParams = ["preferredAnimal"];
+  let searchParams = useSearchParams();
+  searchParams = Object.fromEntries(searchParams.entries());
 
   useEffect(() => {
+    // For each searchParam that is also a personalizationParam,
+    // log it as a trait to the Ninetailed profile.
+    searchParams &&
+      personalizationParams.forEach((param) => {
+        if (searchParams[param]) {
+          identify("", { [param]: searchParams[param] });
+        }
+      });
+    // Additionally, log a page view to Ninetailed.
     page();
   }, [page, pathname]);
+
+  return null;
 };
